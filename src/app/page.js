e7574f661a50e8fb7b092170c8536f4ce1028550
kamilkk00 <kamilkepka22@gmail.com>
2025-05-25 15:33:44 +0200

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+// src/app/page.js
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
