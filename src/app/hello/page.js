e154d8162f49e2743bd7dbf25432cc43a3bb3e2d
kamilkk00 <kamilkepka22@gmail.com>
2025-05-25 16:00:01 +0200

    adding hello page

---
+++ src/app/hello/page.js
@@ -0,0 +1,9 @@
+// src/app/hello/page.js
+
+export default function Home() {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <h1>Welcome in hello page </h1>
+      </div>
+    );
+  }
