9056b3e90d660569f263e5829d2ccb33a234b468
kamilkk00 <kamilkepka22@gmail.com>
2025-05-25 17:17:54 +0200

    change

--- src/app/api/brent/route.js
+++ src/app/api/brent/route.js
@@ -1,7 +1,9 @@
 // app/api/brent/route.js
 import { NextResponse } from 'next/server'
 
-const UPSTREAM_URL = process.env.COMMODITIES_BRENT
+const UPSTREAM_URL =       
+  searchParams.get('url') ||
+  process.env.COMMODITIES_BRENT;
 
 if (!UPSTREAM_URL) {
   throw new Error(
